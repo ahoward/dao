2d8ce844d6926e24b27a1ee324547db707cd3d5e
ara.t.howard <ara.t.howard@gmail.com>
2011-02-16 21:21:22 -0700

    git commit message

--- lib/dao/rails/lib/generators/dao/templates/api.rb
+++ lib/dao/rails/lib/generators/dao/templates/api.rb
@@ -7,6 +7,8 @@ Api =
     }
 
 
+### INSERT YOUR METHOFDS HERE!
+
 
 
   ## this is simply a suggest way to model your api.  it is not required.
@@ -51,11 +53,21 @@ Api =
     def current_user?
       !!effective_user
     end
+
+    def require_effective_user!
+      unless effective_user
+        status :unauthorized
+        return!
+      end
+    end
+    alias_method('require_user!', 'require_effective_user!')
   end
 
 
-unloadable(Api)
+# don't remove this unless you understand this
+#
+  unloadable(Api)
 
-def api(*args, &block)
-  Api.new(*args, &block)
-end
+  def api(*args, &block)
+    Api.new(*args, &block)
+  end
