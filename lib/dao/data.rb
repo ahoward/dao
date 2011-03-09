82f1946e292391a405b8a32d3d6c01a5af515c38
ara.t.howard <ara.t.howard@gmail.com>
2011-03-09 15:03:56 -0700

    bugfix for updating data with model objects directly

--- lib/dao/data.rb
+++ lib/dao/data.rb
@@ -5,61 +5,11 @@ module Dao
 
     def update(*args, &block)
       if args.size==1 and args.first.respond_to?(:to_dao)
-        update(args.first.to_dao)
+        to_dao = args.first.to_dao
+        update(to_dao)
+        return(to_dao)
       end
       super
     end
   end
 end
-
-
-=begin
-    %w( to_dao as_dao ).each do |method|
-      module_eval <<-__, __FILE__, __LINE__
-        def #{ method }(object, *args, &block)
-          case object
-            when Array
-              object.map{|element| Data.#{ method }(element)}
-
-            else
-              if object.respond_to?(:#{ method })
-                object.send(:#{ method }, *args, &block)
-              else
-                object
-              end
-          end
-        end
-      __
-    end
-=end
-
-=begin
-    IdKeys =
-      %w( id uuid guid ).map{|key| [key, key.to_sym, "_#{ key }", "_#{ key }".to_sym]}.flatten
-
-    def id
-      IdKeys.each{|key| return self[key] if has_key?(key)}
-      return nil
-    end
-
-    def has_id?
-      IdKeys.each{|key| return true if has_key?(key)}
-      return false
-    end
-
-    def new?
-      !has_id?
-    end
-
-    def new_record?
-      !has_id?
-    end
-
-    def model_name
-      path.to_s
-    end
-
-    def slug
-      Slug.for(path)
-    end
-=end
