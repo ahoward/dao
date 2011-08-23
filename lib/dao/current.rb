f4004ed306b675833a6cdc20b76f62dd46726448
ara.t.howard <ara.t.howard@gmail.com>
2011-08-23 09:14:21 -0600

    current_XXX support

---
+++ lib/dao/current.rb
@@ -0,0 +1,43 @@
+module Dao::Current
+  Methods = proc do
+    def current_controller
+      @current_controller ||= (
+        if defined?(@controller)
+          @controller
+        else
+          Dao.current_controller || Dao.mock_controller
+        end
+      )
+    end
+
+    %w( request response session ).each do |attr|
+      module_eval <<-__, __FILE__, __LINE__
+        def current_#{ attr }
+          @current_#{ attr } ||= current_controller.instance_eval{ #{ attr } }
+        end
+      __
+    end
+
+    %w( current_user effective_user real_user ).each do |attr|
+      module_eval <<-__, __FILE__, __LINE__
+        def #{ attr }
+          @#{ attr } ||= current_controller.instance_eval{ #{ attr } }
+        end
+      __
+    end
+  end
+
+  ClassMethods = proc do
+    Methods.call()
+  end
+
+  InstanceMethods = proc do
+    Methods.call()
+  end
+
+  def self.included(other)
+    other.send(:instance_eval, &ClassMethods)
+    other.send(:class_eval, &InstanceMethods)
+    super
+  end
+end
